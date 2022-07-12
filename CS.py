eabd0185e52651f6d0e110e4ff30867e510bd609
Jinho Kim <jinhokim.kor@gmail.com>
2022-07-12 10:21:37 +0200

    Flexible for the datapath

--- CS.py
+++ CS.py
@@ -1,4 +1,4 @@
-import os, json, h5py, pathlib
+import os, json, h5py, pathlib, re
 
 import torch
 import sigpy as sp
@@ -8,54 +8,67 @@ from argparse import ArgumentParser
 
 import utils
 
+PATTERN = re.compile("MID\d{5}_FID\d{5}.h5")
+
 
 def cs(args):
+    for root, dir, files in os.walk(args.data_path):
+        for file in files:
+            m = PATTERN.match(file)
+            if m:
+                file = pathlib.Path(root) / file # convert string to pathlib object
+                if args.data_name is None:  # Processing for all dataset
+                    cs_process(file, args)
+                else:  # Processing for specific dataset
+                    if file in args.data_name:
+                        cs_process(file, args)
+
+
+def cs_process(dname, args):
     SENS_EXIST = False
-    for dname in args.data_name:
-        fname = args.data_path / dname
-        with h5py.File(fname, "r") as hf:
-            kspace = hf["kspace"][:]
+    with h5py.File(dname, "r") as hf:
+        kspace = hf["kspace"][:]
 
-        kspace = kspace.astype(np.complex64)
-        ncoil, kx, ky, nz = kspace.shape
-        sens_maps = torch.zeros((nz, ncoil, kx, ky, 2)) # to store all sens_maps
+    kspace = kspace.astype(np.complex64)
+    ncoil, kx, ky, nz = kspace.shape
+    sens_maps = torch.zeros((nz, ncoil, kx, ky, 2))  # to store all sens_maps
 
-        for dataslice in range(nz):
-            folderName = args.save_path / dname.stem
+    for dataslice in range(nz):
+        folderName = args.save_path / dname.stem
 
-            name = f"{fname.stem}_{dataslice}"
-            print(f"{name} start!")
-            kspace_z = kspace[:, :, :, dataslice]
+        name = f"{dname.stem}_{dataslice}"
+        print(f"{name} start!")
+        kspace_z = kspace[:, :, :, dataslice]
 
-            sub_folder = folderName
-            sens_map_pkl_path = sub_folder.parent / 'SensMaps' / f"{sub_folder.stem}_sens_maps.h5"
-            if sens_map_pkl_path.exists():
-                print(f"{sub_folder.stem}_sens_map.h5 is already exist!")
-                SENS_EXIST = True
-                with h5py.File(sens_map_pkl_path, "r") as hf:
-                    sens_map = hf["sens_maps"][dataslice]
-                sens_map = sens_map[...,0] + sens_map[...,1]*1j
-            else:
-                print("Start to estimate sens_map...")
-                sens_map = mr.app.EspiritCalib(kspace_z, device=sp.Device(0), thresh=args.ESPIRiT_threshold).run()
-                sens_map_tensor = utils.to_tensor(sens_map)
-                sens_maps[dataslice] = sens_map_tensor
-                print("Estimate sens_map done...")
-
-            recon_pkl_path = sub_folder.parent / f'CS_pkl' / sub_folder.stem / f"{name}_CS.pkl"
-            if recon_pkl_path.exists():
-                print(f"{name}_CS is already processed!")
-            else:
-                print("Start to reconstruction...")
-                sense_recon = mr.app.L1WaveletRecon(kspace_z.copy(), sens_map.copy(), lamda=args.CS_lambda,
-                                                    device=sp.Device(0)).run()
-                utils.save_result(name, sense_recon, sub_folder=sub_folder)
-                print("Recon done...")
-            print(f"{name} done!\n\n")
-
+        sub_folder = folderName
+        sens_map_pkl_path = sub_folder.parent / 'SensMaps' / f"{sub_folder.stem}_sens_maps.h5"
+        if sens_map_pkl_path.exists():
+            print(f"{sub_folder.stem}_sens_map.h5 is already exist!")
+            SENS_EXIST = True
+            with h5py.File(sens_map_pkl_path, "r") as hf:
+                sens_map = hf["sens_maps"][dataslice]
+            sens_map = sens_map[..., 0] + sens_map[..., 1] * 1j
         else:
-            if not SENS_EXIST:
-                utils.save_sens_maps(sens_maps, sub_folder)
+            print("Start to estimate sens_map...")
+            sens_map = mr.app.EspiritCalib(kspace_z, device=sp.Device(0), thresh=args.ESPIRiT_threshold).run()
+            sens_map_tensor = utils.to_tensor(sens_map)
+            sens_maps[dataslice] = sens_map_tensor
+            print("Estimate sens_map done...")
+
+        recon_pkl_path = sub_folder.parent / f'CS_pkl' / sub_folder.stem / f"{name}_CS.pkl"
+        if recon_pkl_path.exists():
+            print(f"{name}_CS is already processed!")
+        else:
+            print("Start to reconstruction...")
+            sense_recon = mr.app.L1WaveletRecon(kspace_z.copy(), sens_map.copy(), lamda=args.CS_lambda,
+                                                device=sp.Device(0)).run()
+            utils.save_result(name, sense_recon, sub_folder=sub_folder)
+            print("Recon done...")
+        print(f"{name} done!\n\n")
+
+    else:
+        if not SENS_EXIST:
+            utils.save_sens_maps(sens_maps, sub_folder)
 
 
 def build_args(config_json):
@@ -100,8 +113,6 @@ def build_args(config_json):
     data_path = data_path.parent / slurm_job_id / data_path.name
 
     args.data_path = data_path
-    args.data_name = [pathlib.Path(i) for i in os.listdir(data_path)] if args.data_name is None \
-        else [pathlib.Path(args.data_name)]
 
     return args
 
